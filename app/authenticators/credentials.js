92696287963ee272062a715a560f4cd61be84bd3
crystalw20 <crystal@secondstreet.com>
2019-07-30 14:56:43 -0500

    Ember completed

--- app/authenticators/credentials.js
+++ app/authenticators/credentials.js
@@ -1,22 +1,26 @@
-import Base from 'ember-simple-auth/authenticators/base';
-import { inject as service } from '@ember/service';
+import Base from 'ember-simple-auth/authenticators/base'
+import { inject as service } from '@ember/service'
+import ENV from 'rarwe/config/environment'
+
 export default Base.extend({
-ajax: service(),
-async authenticate(username, password) {
-let response = await this.ajax.post('/token', {
-headers: {
-'Accept': 'application/vnd.api+json',
-'Content-Type': 'application/vnd.api+json',
-},
-data: JSON.stringify({
-username,
-password
+  ajax: service(),
+
+  async authenticate (username, password) {
+    let tokenURL = ENV.apiHost ? `${ENV.apiHost}/token` : '/token'
+    let response = await this.ajax.post(tokenURL, {
+      headers: {
+        Accept: 'application/vnd.api+json',
+        'Content-Type': 'application/vnd.api+json'
+      },
+      data: JSON.stringify({
+        username,
+        password
+      })
+    })
+    let { user_email: userEmail, token } = response
+    return { userEmail, token }
+  },
+  async restore (data) {
+    return data
+  }
 })
-})
-let { user_email: userEmail, token } = response;
-return { userEmail, token };
-},
-async restore(data) {
-    return data;
-    }
-});
